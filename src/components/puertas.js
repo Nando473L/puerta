5132b0e70d6f22004b94502ad09de70327116fe6
Nando473L <ferc3455@gmail.com>
2024-02-02 20:50:46 -0600

    first commit

---
+++ src/components/puertas.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function Puertas() {
+  const puertasOpenClose = ['puertaCerrada.jpg', 'puertaAbierta.jpg'];
+  const [puertaSelect, setPuertaSelect] = React.useState(0);
+
+  function botonPuerta() {
+    if (puertaSelect === 0) {
+      setPuertaSelect(1);
+    } else {
+      setPuertaSelect(0);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Abrir y cerrar una puerta</h1>
+      <p>
+        <img src={process.env.PUBLIC_URL + "/imagenes/" + puertasOpenClose[puertaSelect]} alt="puerta" />
+      </p>
+      <p>
+        <input type="button" value="Abrir / Cerrar" onClick={botonPuerta} />
+      </p>
+    </div>
+  );
+}
+
